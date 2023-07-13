32f2e20fac4874638f900d67a72ed7f5af34e79c
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-07-13 06:00:40 +0530

    LO: Use a model

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,5 +1,18 @@
 import { NextResponse } from "next/server";
+import { OpenAI } from "langchain/llms/openai";
+import { PromptTemplate } from "langchain/prompts";
+
+const talkToOpenAI = async function () {
+  const model = new OpenAI({
+    temperature: 2,
+    maxTokens: 500,
+  });
+
+  const res = await model.call("What are you doing?");
+  return res;
+};
 
 export async function GET() {
-  return NextResponse.json("test");
+  const res = await talkToOpenAI();
+  return NextResponse.json(res);
 }
