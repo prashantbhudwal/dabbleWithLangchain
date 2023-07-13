11c2386843c9fa0aa508b5d7d8a6aeac4757d4be
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-07-13 06:06:58 +0530

    LO: using a prompt template

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -7,12 +7,30 @@ const talkToOpenAI = async function () {
     temperature: 2,
     maxTokens: 500,
   });
-
   const res = await model.call("What are you doing?");
   return res;
 };
 
+const usingATemplate = async function () {
+  const firstQuestion = "Can you tell me what do you like about {sport}?";
+
+  const template = new PromptTemplate({
+    template: firstQuestion,
+    inputVariables: ["sport"],
+  });
+
+  const finalPrompt = await template.format({
+    sport: "hockey",
+  });
+
+  const model = new OpenAI({
+    temperature: 0.5,
+  });
+
+  return model.call(finalPrompt);
+};
+
 export async function GET() {
-  const res = await talkToOpenAI();
+  const res = await usingATemplate();
   return NextResponse.json(res);
 }
