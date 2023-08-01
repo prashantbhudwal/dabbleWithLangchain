130b8ec30827b4477938ebb8d5210172917db559
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-08-01 23:54:56 +0530

    Update test route

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,9 +1,11 @@
 export const dynamic = "force-dynamic";
 
 import { NextResponse } from "next/server";
-import { splitMarkdown } from "../lib/textSplitters";
+import { embedDocuments, embedString } from "../lib/embeddings";
+import { pdfLoader } from "../lib/documentLoaders";
+import { Test } from "../lib/vectordb";
 
 export async function GET() {
-  const res = await splitMarkdown();
+  const res = await Test();
   return NextResponse.json(res);
 }
